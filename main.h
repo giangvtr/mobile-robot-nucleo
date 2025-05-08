28aa1201eabd8111362fcc47447506ba599d79bf
Giang Vu <Tra-Giang.Vu@insa-rennes.fr>
2025-05-08 12:21:47 +0200

    Finish TP3

--- main.h
+++ main.h
@@ -15,6 +15,15 @@
 #include "gpio.h"
 #include "systick.h"
 #include "stm32_periph.h"
+#include "moteur.h"
+
+#define INDEX_50ms 500
+
+volatile char fin_reception = 0;
+volatile unsigned short index_time=0;
+volatile unsigned short nbr_un = 0;
+volatile unsigned short distance = 0;
+volatile unsigned short nbrUnFinal = 0;
 
 typedef struct {
 	unsigned short nbrIT;	
