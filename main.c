e28147075182aa4aff1ed2885b6e74a45c27ddb7
Giang Vu <Tra-Giang.Vu@insa-rennes.fr>
2025-05-13 12:10:48 +0200

    Debug initIRTimer and IRQHandler

--- main.c
+++ main.c
@@ -48,107 +48,113 @@ unsigned char getKeyNumber(void){
 	return key;
 }
 
-void initIRTimer(void){
-	/* Configure in Input capture mode */
-	/* Enable peripheral Clock for TIM5-CH2 (APB1 bus) et pin PA1 */
-	/* Enable input pin for PA3 */
-	RCC -> APB1ENR |= RCC_APB1ENR_TIM5EN ;
-	RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN ;
+  void initIRTimer(void){
+    /* Configure in Input capture mode */
+    /* Enable peripheral Clock for TIM5-CH2 (APB1 bus) et pin PA1 */
+    /* Enable input pin for PA3 */
+    RCC -> APB1ENR |= RCC_APB1ENR_TIM5EN ;
+    RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN ;
 
-	/* GPIO pin config . alternate function 1 to connect TIM5 - CH2 to PA1 */
-	GPIOA -> MODER &= ~ (GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE3_Msk) ;
-	GPIOA -> MODER |= GPIO_MODE_AF_PP << GPIO_MODER_MODE1_Pos;
+    /* GPIO pin config . alternate function 1 to connect TIM5 - CH2 to PA1 */
+    GPIOA -> MODER &= ~ (GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE3_Msk) ;
+    GPIOA -> MODER |= GPIO_MODE_AF_PP << GPIO_MODER_MODE1_Pos;
 
-	/* 5. Configurer les résistances de Pull-Up / Pull-Down */
-	GPIOA->PUPDR &= ~( GPIO_PUPDR_PUPDR1_Msk); // Clear bits
-	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_1;      // Mode pull-up
-	// 00: GPIO_NOPULL | 01: GPIO_PULLUP
-	// 10: GPIO_PULLDOWN | 11: Reserved
+    /* Configurer les résistances de Pull-Up / Pull-Down */
+    GPIOA->PUPDR &= ~( GPIO_PUPDR_PUPDR1); // Clear bits
+    GPIOA->PUPDR |= GPIO_PUPDR_PUPDR1_0;      // Mode pull-up
+    // 00: GPIO_NOPULL | 01: GPIO_PULLUP
+    // 10: GPIO_PULLDOWN | 11: Reserved
 
-	GPIOA ->AFR[0] &= ~ GPIO_AFRL_AFSEL1 ;                    // Reset AF2 (pin Low in AFR[0])
-	GPIOA ->AFR[0] |= GPIO_AF1_TIM5 << GPIO_AFRL_AFSEL1_Pos ;
+    GPIOA ->AFR[0] &= ~ GPIO_AFRL_AFSEL1 ;                    // Reset AF2 (pin Low in AFR[0])
+    GPIOA ->AFR[0] |= GPIO_AF2_TIM5 << GPIO_AFRL_AFSEL1_Pos ; //On aattend AFRL = 0xX2X ????
 
-	/* Clock definition in Reset mode (100) */
-	//We want it to automatically calculate the delta then reset the counter
-	TIM5->SMCR &= ~TIM_SMCR_SMS_Msk ;
-	TIM5->SMCR |= (0b100 << TIM_SMCR_SMS_Pos);  //Slave mode - reset mode
 
-	//Select the source of trigger (l'entree sur laquelle tu captures les fronts (ici est le signal IR)
-	TIM5->SMCR &= ~TIM_SMCR_TS_Msk;
-	TIM5->SMCR |= TIM_CLOCKSOURCE_TI2; //Capture channel 2 = TI2 input
+    /* Clock definition in Reset mode (100 = 0x8U) */
+    //We want it to automatically calculate the delta then reset the counter
+    TIM5->SMCR &= ~(TIM_SMCR_SMS_Msk) ;
+    TIM5->SMCR |= (0x4U << TIM_SMCR_SMS_Pos);  //Slave mode reset mode
 
-	/* Counter Mode definition */
-	TIM5->CR1 &= ~( TIM_CR1_DIR // Up counting
-					| TIM_CR1_CMS ) ; // Edge - aligned
-	/* Prescaler value definition (pas definition pour ARR)*/
-	//On veut la periode entre 2 fronts descendants est 13,5 ms
-	TIM5->PSC = 89; //resolution = 1MHz = 1us
-	TIM5->ARR = 99999;
+    //Select the source of trigger (l'entree sur laquelle tu captures les fronts (ici est le signal IR)
+    TIM5->SMCR &= ~TIM_SMCR_TS_Msk;
+    TIM5->SMCR |= TIM_CLOCKSOURCE_TI2; //Capture channel 2 = TI2 input
 
-	/* Input capture mode - channel configuration */
-	TIM5 -> CCMR1 &= ~TIM_CCMR1_CC2S ; // CC1 channel bit is cleared
-	TIM5 -> CCMR1 |= TIM_ICSELECTION_DIRECTTI ; // and IC1 mapped on TI1.
-	TIM5 -> CCMR1 &= ~TIM_CCMR1_IC2F ; // No filter for ch2
-	TIM5 -> CCMR1 &= ~ TIM_CCMR1_IC2PSC ; // No prescaler, capture each time an edge is detected
+    /* Counter Mode definition */
+    TIM5->CR1 &= ~( TIM_CR1_DIR // Up counting
+                                    | TIM_CR1_CMS) ; // Edge - aligned
+    /* Prescaler value definition (pas definition pour ARR)*/
+    //On veut la periode entre 2 fronts descendants est 13,5 ms
+    TIM5->PSC = 1012; //resolution = 5.625 us
+    TIM5->ARR = 10000;
 
-	/* Input capture enable on channel 2 */
-	TIM5 -> CCER &= ~(TIM_CCER_CC2P | TIM_CCER_CC2NP ) ; // Falling edge detect
-	TIM5 -> CCER |= TIM_INPUTCHANNELPOLARITY_FALLING << TIM_CCER_CC2P_Pos
-					| TIM_CCER_CC2E ; // Capture Enable
+    /* Input capture mode - channel configuration */
+    TIM5 -> CCMR1 &= ~TIM_CCMR1_CC2S ; // CC2 channel bit is cleared
+    TIM5 -> CCMR1 |= TIM_CCMR1_CC2S_0 ; // and IC2 mapped on TI2.
+    TIM5 -> CCMR1 &= ~TIM_CCMR1_IC2F ; // No filter for ch2
+    TIM5 -> CCMR1 &= ~TIM_CCMR1_IC2PSC ; // No prescaler, capture each time an edge is detected
 
-	/* Enable interrupt flag when capture occurs on channel 2 */
-	TIM5 -> DIER |= TIM_DIER_CC2IE ; // Enable interrupt when falling edge captured
-	TIM5 -> SR = ~TIM_SR_CC2IF ; // Clear the Capture event flag for channel 2
-	// Enable update interrupt (triggered by counter overflow or update event)
-	TIM5->DIER |= TIM_DIER_UIE;
-	TIM5->SR &= ~(TIM_SR_UIF);
+    /* Input capture enable on channel 2 */
+    TIM5 -> CCER &= ~(TIM_CCER_CC2P | TIM_CCER_CC2NP ) ; // Falling edge detect
+    TIM5 -> CCER |= TIM_CCER_CC2P
+                                    | TIM_CCER_CC2E ; // Capture Enable
 
-	/* Enable interrupt in Interrupt controller */
-	NVIC_SetPriority(TIM5_IRQn ,3) ;
-	NVIC_ClearPendingIRQ(TIM5_IRQn) ;
-	NVIC_EnableIRQ(TIM5_IRQn) ;
+    /* Enable interrupt flag when capture occurs on channel 2 */
+    TIM5 -> DIER |= TIM_DIER_CC2IE ; // Enable interrupt when falling edge captured
+    TIM5 -> SR = ~TIM_SR_CC2IF ; // Clear the Capture event flag for channel 2
+    // Enable update interrupt (triggered by counter overflow or update event)
+    TIM5->DIER |= TIM_DIER_UIE;
+    TIM5->SR &= ~(TIM_SR_UIF);
+
+    //Enable counter
+    TIM5->CR1 |= TIM_CR1_CEN;
+
+    /* Enable interrupt in Interrupt controller */
+    NVIC_SetPriority(TIM5_IRQn ,3) ;
+    NVIC_ClearPendingIRQ(TIM5_IRQn) ;
+    NVIC_EnableIRQ(TIM5_IRQn) ;
 }
 
 void TIM5_IRQHandler(void){
-	//interruption type debordement
-	if (TIM5->SR & TIM_SR_UIF) {
-		//Case an overflows clock
-		//Means that the IR Reception is taking for too long
-		TIM5->SR &= ~(TIM_SR_UIF);  // Clear pending flag
-		// Vider le buffer pour recommencer
-		bit_index = 0;
-		fin_reception = 0;
-		code_IR = 0;
-		return;
-	}
-	else if (TIM5->SR & TIM_SR_CC2IF) {
-		//Case an interrupt is detected (front descendant)
-		TIM5->SR &= ~(TIM_SR_CC2IF);
-		//Detect bit value and add in a buffer
-		uint32_t pulse_duration = TIM5->CCR2;  // Valeur du timer au front descendant
-		if(pulse_duration > 12000 && pulse_duration < 15000 && bit_index == 0){
-		//This is the SOF signal, nothing is put into the code_IR buffer
-		//Use 12000 since the resolution is in 1 us, we want a frame that covers 13.5 ms
-			bit_index = 0;
+    //interruption type debordement
+    if (TIM5->SR & TIM_SR_UIF) {
+        //Case an overflows clock
+        //Means that the IR Reception is taking for too long
+        TIM5->SR &= ~(TIM_SR_UIF);  // Clear pending flag
+        // Vider le buffer pour recommencer
+        bit_index = 0;
+        fin_reception = 0;
+        code_IR = 0;
+        return;
+    }
+    else if (TIM5->SR & TIM_SR_CC2IF) {
+        //Case an interrupt is detected (front descendant)
+        TIM5->SR &= ~(TIM_SR_CC2IF);
+        //Detect bit value and add in a buffer
+        uint32_t pulse_duration = TIM5->CCR2;  // Valeur du timer au front descendant
+        if(pulse_duration > 2200 && pulse_duration < 2600 && bit_index == 0){
+        //This is the SOF signal, nothing is put into the code_IR buffer
+        //Use 12000 since the resolution is in 5.625 us, we want a frame that covers 13.5 ms => 2400 fois
+            bit_index = 0;
 
-		} else if (bit_index < MAX_BITS) {
-			if (pulse_duration > 1000 && pulse_duration < 2000) {
-				bit_buffer[bit_index++] = 0;
-			} else if (pulse_duration > 2000 && pulse_duration < 2500) {
-				bit_buffer[bit_index++] = 1;
-			}
-		}
-		if (bit_index >= MAX_BITS) {
-			// Convertir le buffer en entier 32 bits
-			code_IR = 0; //Clear the old IR_command
-			for (int i = 0; i < 32; i++) {
-				code_IR <<= 1;
-				code_IR |= bit_buffer[i];
-			}
-			fin_reception = 1;
-			bit_index = 0;
-		}
-	}
+        } else if (bit_index < MAX_BITS) {
+            //Bit 0: 200 fois 5.625 us
+            if (pulse_duration > 150 && pulse_duration < 300) {
+                    bit_buffer[bit_index++] = 0;
+            //Bit 1: 400 fois
+            } else if (pulse_duration > 300 && pulse_duration < 500) {
+                    bit_buffer[bit_index++] = 1;
+            }
+        }
+        if (bit_index >= MAX_BITS) {
+            // Convertir le buffer en entier 32 bits
+            code_IR = 0; //Clear the old IR_command
+            for (int i = 0; i < 32; i++) {
+                    code_IR <<= 1;
+                    code_IR |= bit_buffer[i];
+            }
+            fin_reception = 1;
+            bit_index = 0;
+        }
+    }
 }
 
 	
