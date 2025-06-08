846eef9d5cb078d887e032dc6d8f938f368f4b32
Giang Vu <105714666+giangvtr@users.noreply.github.com>
2025-06-08 15:53:49 +0200

    Update ultrasound.c

--- ultrasound.c
+++ ultrasound.c
@@ -43,25 +43,27 @@ void ComputeDistance(void){
 //PA5 need to be in input mode
 void initGPIORadar(void){
 	//=====Init GPIO=======
+	/*
+        PA1 : logical output - PWM (period echantillonage) - TIM2-CH2
+	PA5: logical output - Period interruption (toogle from PA1 overflow)
+ 	PA12 : output numerical - Trig
+	PA8 : entree numerique - Echo
+ 	*/
 
 	RCC->AHB1ENR = RCC_AHB1ENR_GPIOAEN;
 
 	//MODER Mode Output
-	GPIOA->MODER &= ~(GPIO_MODER_MODER8_Msk
-										| GPIO_MODER_MODER12_Msk
-										| GPIO_MODER_MODER1_Msk
-										| GPIO_MODER_MODER5_Msk);
+	GPIOA->MODER &= ~(GPIO_MODER_MODER8_Msk | GPIO_MODER_MODER12_Msk
+			| GPIO_MODER_MODER1_Msk | GPIO_MODER_MODER5_Msk);
 
 	GPIOA->MODER |= (GPIO_MODE_AF_PP << GPIO_MODER_MODER1_Pos
-								| GPIO_MODE_OUTPUT_PP << GPIO_MODER_MODER5_Pos
-								| GPIO_MODE_OUTPUT_PP << GPIO_MODER_MODER12_Pos
-	              | GPIO_MODE_INPUT << GPIO_MODER_MODER8_Pos);
+			| GPIO_MODE_OUTPUT_PP << GPIO_MODER_MODER5_Pos
+			| GPIO_MODE_OUTPUT_PP << GPIO_MODER_MODER12_Pos
+	              	| GPIO_MODE_INPUT << GPIO_MODER_MODER8_Pos);
 
 	//OTYPER Mode push-pull = 0 (push-pull)
-	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_8
-										| GPIO_OTYPER_OT_12
-										| GPIO_OTYPER_OT_1
-										| GPIO_OTYPER_OT_5);
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT_8 | GPIO_OTYPER_OT_12
+			| GPIO_OTYPER_OT_1 | GPIO_OTYPER_OT_5);
 
 	//Reset OSPEEDR a 0 puis set a medium
 	GPIOA->OSPEEDR &= ~(GPIO_OSPEEDR_OSPEED8_Msk
@@ -98,38 +100,42 @@ void initTimerRadar(void){
 	//Configure the counter
 	TIM2->SMCR &= ~TIM_SMCR_SMS;
 	TIM2->CR1 &= ~(TIM_CR1_DIR | TIM_CR1_CMS);  //Count up and edge-aligned
+	TIM2->CR1 |= TIM_CR1_ARPE
 
 	TIM2->PSC=179; //On veut 1MHz depuis 180Hz (formule CM4 pg 6)
 	TIM2->ARR=99; //on veut 100u => 10kHz => ARR
 
 	//Timer sur channel 2 (PA1 - AF01)
-	TIM2->CCMR1 &= ~(TIM_CCMR1_CC2S); //CC2 channel is mode output (CC2S = 0)
+	TIM2->CCMR1 &= ~(TIM_CCMR1_CC2S | TIM_CCMR1_OC2M_Msk); //CC2 channel is mode output (CC2S = 0)
 
-	TIM2->CCMR1 &= ~(TIM_CCMR1_OC2M_Msk);
 	TIM2->CCMR1 |= TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2M_1; //TIM_CCMR1_OC2PE
 							//| TIM_OCMODE_PWM1;  // = 6 = mode PWM1
 
-	//Manage output preload (PAS SURE - disabled)
-	TIM2->CCMR1 &= ~(TIM_CCMR1_OC2PE_Msk);
+	//Manage output preload enable
+	TIM2->CCMR1 |= TIM_CCMR1_OC2PE;
 
 	//Enable output compare
-	TIM2->CCER &= ~(TIM_CCER_CC2E_Msk);
+	
+	TIM2->CCER &= ~(TIM_CCER_CC2E_Msk | TIM_CCER_CC2NP);
 	TIM2->CCER |= TIM_CCER_CC2E;
 
 	//Set duty cycle (50%)
-	TIM2->CCR2 = 50;
+	TIM2->CCR2 = 50-1;
 
+	//======= INTERRUPTION ==========
 	//Enable the generation of an interrupt when the timer overflows or reaches the update event
-	TIM2->DIER |= TIM_DIER_UIE;
-	TIM2->SR &= ~(TIM_SR_UIF);
+	TIM2->DIER |= (TIM_DIER_UIE | TIM_DIER_CC2IE);
+	
+	TIM2->SR &= ~(TIM_SR_UIF);	//Overflow flag
+	TIM2->SR &=~TIM_SR_CC2IF;	//Capture/compare flag
 
 	//Enable timer
 	TIM2->CR1 |= TIM_CR1_CEN;
-	TIM2->CR1 |=TIM_CR1_ARPE;
 
 	NVIC_EnableIRQ(TIM2_IRQn);
 	NVIC_SetPriority(TIM2_IRQn, 3);
 	NVIC_ClearPendingIRQ(TIM2_IRQn);
+	__enable_irq();
 }
 
 void TIM2_IRQHandler(void) {
